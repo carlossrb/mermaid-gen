efce70941b59c079f5347ab92c2a6d257d08db98
Carlos Brito <carlos.ribeiro.brito@hotmail.com>
2023-12-11 13:09:15 -0300

    fix: change permissions

--- mermaid.mjs
+++ mermaid.mjs
@@ -1,42 +1,41 @@
-import { exec, execSync } from 'child_process';
-import fs from 'fs';
-import { run } from '@mermaid-js/mermaid-cli';
-import path, { dirname } from 'path';
+import { exec, execSync } from 'child_process'
+import fs from 'fs'
+import { run } from '@mermaid-js/mermaid-cli'
+import path, { dirname } from 'path'
 
-const __dirname = dirname('');
-const arch = process.platform + process.arch;
+const __dirname = dirname('')
+const arch = process.platform + process.arch
 const platform = {
   darwinarm64: 'mac_arm',
   darwinx64: 'mac',
   linuxx64: 'linux',
   win32x64: 'win64',
-  win32ia32: 'win32'
-};
+  win32ia32: 'win32',
+}
 
-const mermaidDiagramPath = path.resolve(__dirname, 'diagrams/mermaid');
-const mermaidDiagramImages = path.resolve(__dirname, 'diagrams/images');
-
+const mermaidDiagramPath = path.resolve(__dirname, 'diagrams/mermaid')
+const mermaidDiagramImages = path.resolve(__dirname, 'diagrams/images')
+console.log(mermaidDiagramPath, mermaidDiagramImages)
 async function generateDiagrams() {
   try {
-    console.log('Creating diagrams in progress...');
+    console.log('Creating diagrams in progress...')
+    const cmd = `npx @puppeteer/browsers install chromium --platform ${platform[arch]}`
+    console.log(cmd)
+    const { stdout } = await execPromise(cmd)
+    const [, chromiumPath] = stdout.split(' ')
 
-    const { stdout } = await execPromise(
-      `npx @puppeteer/browsers install chromium --platform ${platform[arch]}`
-    );
-    const [, chromiumPath] = stdout.split(' ');
-
-    const files = fs.readdirSync(mermaidDiagramPath);
-    console.log(`Found ${files.length} mermaid files. Generating diagrams...`);
+    const files = fs.readdirSync(mermaidDiagramPath)
+    console.log(`Found ${files.length} mermaid files. Generating diagrams...`)
 
     const mermaidDiagrams = files.map((file, idx) =>
       generateDiagram(file, chromiumPath)
-    );
-    await Promise.all(mermaidDiagrams);
+    )
+    await Promise.all(mermaidDiagrams)
 
-    console.log('All diagrams have been generated successfully.');
-    execSync(`rm -rf chromium`);
+    console.log('All diagrams have been generated successfully.')
+    execSync(`rm -rf chromium`)
   } catch (error) {
-    console.error('An error occurred:', error.message);
+    console.error('An error occurred:', error.message)
   }
 }
 
@@ -44,28 +43,25 @@ function execPromise(command) {
   return new Promise((resolve, reject) => {
     exec(command, (error, stdout, stderr) => {
       if (error) {
-        reject(error);
-        return;
+        reject(error)
+        return
       }
-      resolve({ stdout, stderr });
-    });
-  });
+      resolve({ stdout, stderr })
+    })
+  })
 }
 
 async function generateDiagram(file, chromiumPath) {
-  const inputPath = path.join(mermaidDiagramPath, file);
-  const outputPath = path.join(
-    mermaidDiagramImages,
-    file.replace('mmd', 'png')
-  );
+  const inputPath = path.join(mermaidDiagramPath, file)
+  const outputPath = path.join(mermaidDiagramImages, file.replace('mmd', 'png'))
 
-  console.log(`Processing ${file}...`);
+  console.log(`Processing ${file}...`)
   await run(inputPath, outputPath, {
     puppeteerConfig: {
       headless: 'new',
-      executablePath: chromiumPath.trim()
-    }
-  });
+      executablePath: chromiumPath.trim(),
+    },
+  })
 }
 
-generateDiagrams();
+generateDiagrams()
